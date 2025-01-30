60fb7a81fb42764f8c4125b31630ef76fbf1a964
my4776 <my4776@bs.amalnet.k12.il>
2025-01-30 23:19:10 +0200

    misiion 1, this is crap

--- app/src/main/java/com/example/homework5/MainActivity.java
+++ app/src/main/java/com/example/homework5/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.homework5;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    
+    Button btn;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn.findViewById(R.id.btn);
+        tv.findViewById(R.id.tv);
+    }
+
+    public void Albert(View view) {
+        tv.setText("Oh, yea, I’ve been clicked!");
     }
 }
